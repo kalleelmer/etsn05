f04a930ba947e8d338709368a5e4432b1a52790d
villzor <ine11vsj@student.lu.se>
2014-10-19 19:44:59 +0200

    Rättade till insert-funktionen

--- ETSN05/BaseBlockSystem/src/database/TimeReport.java
+++ ETSN05/BaseBlockSystem/src/database/TimeReport.java
@@ -2,9 +2,8 @@ package database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.List;
+import java.sql.Date;
 
 import database.Member.Role;
 
@@ -20,43 +19,53 @@ public class TimeReport extends Entity {
 	public final int PROJECT_ID;
 	public final Member.Role ROLE;
 	public final int ACTIVITY_TYPE;
+	public final Date DATE;
 	public final int DURATION;
 	public final String SIGNER;
 	
-//	Här ska Date skickas med....
 	public TimeReport(int id, String userName, int projectID, Member.Role role,
-			int activityType, int duration, String signer) {
+			int activityType, Date date, int duration, String signer) {
 		ID = id;
 		USERNAME = userName;
 		PROJECT_ID = projectID;
 		ROLE = role;
 		ACTIVITY_TYPE = activityType;
+		DATE = date;
 		DURATION = duration;
 		SIGNER = signer;
 	}
 	
-//	public static TimeReport getByID(int id) {
-//		String selectQuery = "SELECT FROM timeReports WHERE id=" + id;
-//		ResultSet rs = selectQuery(selectQuery);
-//		try {
-//			if (!rs.next()) {
-//				return null;
-//			} else {
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public static TimeReport getByID(int id) throws SQLException, Exception {
+		String timeReportQuery = "SELECT * FROM timeReports WHERE id=" + id;
+		ResultSet timeReportSet = selectQuery(timeReportQuery);
+		if (!timeReportSet.next()) {
+			return null;
+		}
+		TimeReport timeReport = new TimeReport(timeReportSet.getInt("id"),
+				timeReportSet.getString("user"),
+				timeReportSet.getInt("project"), Role.valueOf(timeReportSet
+						.getString("role")),
+				timeReportSet.getInt("activityType"),
+				timeReportSet.getDate("date"),
+				timeReportSet.getInt("duration"),
+				timeReportSet.getString("signer"));
+		return timeReport;
+	}
 	
+	public static List<TimeReport> get(User user, Project project,
+			boolean signed, String start, String end, Role role,
+			int activityType) throws IllegalArgumentException {
+		Date startDate = Date.valueOf(start);
+		Date endDate = Date.valueOf(end);
+		
+		return null;
+	}
+
 	/**
 	 * Inserts a time report to the database
 	 */
 	public void insert() throws SQLException, Exception{
-		Calendar cal = Calendar.getInstance();
-		cal.getTime();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		String date = sdf.format(new Date());
-		String addQuery = "INSERT INTO timeReports(id,user,project,role,activityType,date,duration,signer) VALUES("
+		String addQuery = "INSERT INTO timeReports VALUES("
 				+ ID
 				+ ",'"
 				+ USERNAME
@@ -67,7 +76,7 @@ public class TimeReport extends Entity {
 				+ "',"
 				+ ACTIVITY_TYPE
 				+ ",'"
-				+ date
+				+ DATE
 				+ "',"
 				+ DURATION
 				+ ",'"
@@ -101,5 +110,20 @@ public class TimeReport extends Entity {
 	public void delete() throws SQLException, Exception {
 		String deleteQuery = "DELETE FROM timeReports WHERE id=" + ID;
 		query(deleteQuery);
+	}
+	
+	public static void main(String[] args) {
+		Date date = Date.valueOf("2014-08-12");
+		TimeReport r1 = new TimeReport(2,"Goran",2,Role.developer,13,date,200,"ville");
+		System.out.print(r1.DATE.toString());
+		try {
+			r1.insert();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
