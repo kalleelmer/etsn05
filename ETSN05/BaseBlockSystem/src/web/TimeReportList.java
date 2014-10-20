b424d76ca23c155c7f6093ae9310f2c864812151
fredrik-nylund <fredrik.nylund@hotmail.se>
2014-10-20 20:36:09 +0200

    funktionaliteten klar för create new time report

--- ETSN05/BaseBlockSystem/src/web/TimeReportList.java
+++ ETSN05/BaseBlockSystem/src/web/TimeReportList.java
@@ -2,6 +2,9 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,40 +14,104 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import database.Member;
+import database.Member.Role;
+import database.Project;
+import database.TimeReport;
+import database.User;
+
 @WebServlet("/TimeReportList")
 public class TimeReportList extends servletBase {
 
-	/**
-	 * Instantiate a time report list servlet
-	 */
 	public TimeReportList() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	 /**
      * Generates a form for Time Report  List. 
+     * @param logged in user
      * @return HTML code for the form
+	 * @throws Exception 
+	 * @throws SQLException 
+	 * @throws SecurityException 
      */
-    protected String timeReportListRequestForm() {
-    	String html = "<html><body><p>Time Report List:</p>";
-    	String[] tableRow1 = {"","User","Project","Start date","End date","Summarize by"};
-    	String[] tableRow2 = {"Display",dropDownMenu(tableRow1,"wow","wow"),dropDownMenu(tableRow1,"wow","wow")
-    			,dropDownMenu(tableRow1,"wow","wow"),dropDownMenu(tableRow1,"wow","wow"),dropDownMenu(tableRow1,"wow","wow")};
+    protected String timeReportListRequestForm(String userName) {
+    	//Retrieving projects that user is member in
+    	List<Project> projects=null;
+    	try {
+    		projects=Project.getByUser(userName);
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}
+    	
+        List<String> users = Arrays.asList("", "User", "Project","Start date","End date","Summarize by");
+
+    	
+    	String html = "";
+    	List<String> summary = Arrays.asList("Week","Activity Type","Project");
+        List<String> tableRow1 = Arrays.asList("", "User", "Project","Start date","End date","Summarize by");
+    	List<String> tableRow2 = Arrays.asList("Display",dropDownMenu(users,"name",""),
+    			dropDownMenu(projects,"project","all projects"),
+    			"<input type="+formElement("text") +"name="+formElement("startDate")+">",
+    			"<input type="+formElement("text") +"name="+formElement("endDate")+">",
+    			dropDownMenu(summary,"summary","all"));
     	String[] tableRow3 = {"ID","Project","User","Activity type","Date","Duration"};
-    	html += "<table style="+formElement("width:80%")+tableRow(tableRow1)+"</tr>"+tableRow(tableRow2);
-    	html += "<td><input type=" + formElement("submit") + "value=" + formElement("Update") + "></td></tr>";
-    	html += tableRow(tableRow3)+"</tr>";
+    	html = "<p> <form name=" + formElement("input");
+    	html += " method=" + formElement("get")+">";
+    	html += "<table style="+formElement("width:80%")+tableRow(tableRow1)+"</tr><tr>"+tableRow(tableRow2);
+    	html += "<td><input type=" + formElement("submit") + "value=" + formElement("update") + "></td></tr>";
     	html += "</table>";
+    	html += "</form>";
     	return html;
     			
     }
     
-    /**
-     * Generates HTML code for table rows
-     * @param list String array
-     * @return String containing HTML syntax
+    private List<User> getUserList(String userName) throws SQLException, Exception {
+    	return null;
+	}
+
+	protected String newTimeReportRequestForm(String userName) {    
+    	//Retrieving projects that user is member in
+		List<Project> projects=null;
+    	try {
+    		projects=Project.getByUser(userName);
+    	}catch(Exception e){
+			e.printStackTrace();
+    	}
+    	
+    	String html = "";
+        List<String> activityType = Arrays.asList("1", "2","3","4");
+        List<String> tableRow1 = Arrays.asList("Project", "Activity type","Date","Duration");
+    	List<String> tableRow2 = Arrays.asList(dropDownMenu(projects,"createNewProject","select project"),
+    			dropDownMenu(activityType,"createNewType","select activity type"),
+    			"<input type="+formElement("text") +"name="+formElement("createNewDate")+">",
+    			"<input type="+formElement("text") +"name="+formElement("createNewDuration")+">");
+    	html = "<p> <form name=" + formElement("input");
+    	html += " method=" + formElement("get")+">";
+    	html += "<table style="+formElement("width:80%")+tableRow(tableRow1)+"</tr><tr>"+tableRow(tableRow2);
+    	html += "<td><input type=" + formElement("submit") + "name=" + formElement("create") +"value="+formElement("process")+ "></td></tr>";
+    	html += "</table>";
+    	html += "</form>";
+    	return html;
+    			
+    }
+    
+    
+	 /**
+     * Generates a form for Time Report  List. 
+     * @param logged in user
+     * @return HTML code for the form
      */
-    private String tableRow(String[] list){
+    protected String generateTimeReportList(String userName) {
+    	return null;
+    			
+    }
+    
+    /**
+     * Generates a table row with elements in list. Have to be enclosed with </tr>
+     * @param list
+     * @return
+     */
+    private String tableRow(List<String> list){
     	String html = "<tr>";
     	for (String s:list){
     		html +="<td>" +s + "</td>";
@@ -52,62 +119,130 @@ public class TimeReportList extends servletBase {
     	return html;
     }
     
-    /**
-     * TEMPORARY: REMOVE BEFORE FINAL RELEASE
-     */
-    private List<String> tmp(){
-    	LinkedList<String> tmp = new LinkedList<String>();
-    	tmp.add("hej");
-    	tmp.add("he3dj");
-    	tmp.add("hedj");
-    	return tmp;
+
+    private <E> String getString(E s){
+    	if (s instanceof String){
+    		return (String) s;
+    	}else if(s instanceof User){
+    		return ((User) s).USERNAME;
+    	}else if(s instanceof Project){
+    		return ((Project) s).ID + "- "+ ((Project) s).NAME;
+    	}
+    	else{
+    		return "";
+    	}
     }
     
-    /**
-     * Produces HTML code for the drop down menu
-     * @param list Array of drop down options
-     * @param name Name of the field to select
-     * @param intro Start value
-     * @return String containing HTML syntax
-     */
-    private String dropDownMenu(String[] list, String name,String intro){
+    private <E> String dropDownMenu(List<E> list, String name,String intro){
     	String html = "<select name ="+formElement(name)+">";
     	html += "<option value="+intro+">"+intro+"</option>";
-    	for (String s:list){
-    		String f = formElement(s);
+    	if (list==null){
+        	html += "</select>";
+    		return html;
+    	}
+    	for (E s:list){
+    		String f = formElement(getString(s));
     		html += "<option value="+f+">"+f+"</option>";
     	}
     	html += "</select>";
     	return html;
     }
     
-    private String generateList(List<String> list, String name,String intro){
-    	return "";
-    }
+ 
+    
+  
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-    	String myName = "";
+
     	HttpSession session = request.getSession(true);
-    	Object nameObj = session.getAttribute("name");
-    	if (nameObj != null)
-    		myName = (String)nameObj;  // if the name exists typecast the name to a string
-		int state;
+    	String userName = (String) session.getAttribute("name");
+    	Object update = request.getParameter("update");
+    	String create = (String) request.getParameter("create");
+    	
+    	String createNewProject = request.getParameter("createNewProject");
+    	String createNewType = request.getParameter("createNewType");
+    	String createNewDate = request.getParameter("createNewDate");
+    	String createNewDuration = request.getParameter("createNewDuration");
 
 		PrintWriter out = response.getWriter();
 		out.println(getPageIntro());
+    	out.println("<html><body><p>Time Report List:</p>");    	
+
     	
-		
-		if (!loggedIn(request))
-			response.sendRedirect("LogIn");
-		else
-			out.println(timeReportListRequestForm());
-			out.println("<p><a href =" + formElement("NewTimeReport") + "> Create new time report </p>");
-			out.println("<p><a href =" + formElement("LogIn") + "> Log out </p>");
-			out.println("</body></html>");
+		// check that the user is logged in
+			if (!loggedIn(request)){
+				response.sendRedirect("LogIn");
+			}else{
+				if(update==null&&create==null){
+					out.println(timeReportListRequestForm(userName));
+				}else if(create!=null){
+					if(create.equals("init")){
+						out.println(newTimeReportRequestForm(userName));
+					}else if (create.equals("process")){
+						out.println(createTimeReport(userName,createNewProject,createNewType,createNewDate,createNewDuration));
+						out.println(newTimeReportRequestForm(userName));
+					}
+				}else if(update!=null){
+					
+				}
+				out.println("<p><a href =" + formElement("TimeReportList?create=init")+"> Create new time report </p>");
+				out.println("<p><a href =" + formElement("TimeReportList")+"> Time report List</p>");
+				out.println("<p><a href =" + formElement("LogIn") + "> Log out </p>");
+				out.println("</body></html>");
+			}
 		
 	}
     
-    
+
+	private String createTimeReport(String userName,String project,String activityType,String date,String duration) {
+		Date dateObj=null;
+		int durationInt = 0;
+		int projectID = 0;
+		try{
+			dateObj = Date.valueOf(date);
+		}catch(Exception e){
+			return "<p>Date format: yyyy-mm-dd</p>";
+		}
+		try{
+			durationInt = Integer.parseInt(duration);
+		}catch(Exception e){
+			return "<p>Fill in duration (minutes)</p>";
+		}
+		
+		if (!project.equals("select")){
+			String[] tmp = project.split("-",2);
+			projectID = Integer.parseInt(tmp[0]);
+		}else{
+			return "<p>Select a project</p>";
+		}
+		if (activityType.equals("select")){
+			return "<p>Select a activity type</p>";
+		}
+		TimeReport newTimeReport = new TimeReport(1,userName, projectID, getRole(userName,projectID),
+				Integer.parseInt(activityType), dateObj, durationInt, null);
+		try {
+			newTimeReport.insert();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "<p>Time report created</p>";
+	}
+	
+    private Role getRole(String userName, int project){
+    	try {
+			for (Member m :Member.getMembers(project)){
+				if (m.USERNAME==userName){
+					return m.ROLE;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return null;
+    }
+	
+
+	
 
 }
