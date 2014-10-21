87fcfa8c7cfd7adf6331b92dbb9c177d6a68fe29
etsn05 <dahlstrom.philip@gmail.com>
2014-10-21 23:42:56 +0200

    jävla html skit

--- ETSN05/BaseBlockSystem/src/web/MemberList.java
+++ ETSN05/BaseBlockSystem/src/web/MemberList.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+@WebServlet("/MemberList")
+
 
 public class MemberList extends servletBase {
 
@@ -19,7 +22,7 @@ public class MemberList extends servletBase {
 	}
 
 	protected String memberListRequestform() {
-		return "";
+		return String html = "<html><body><p> Test<p>";
 	}
 	
 	protected void goGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
