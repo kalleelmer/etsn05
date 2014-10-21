aaab8a3d8f7188a834a43c144a38eae8431f075c
SeBBBe <sjlfabian@gmail.com>
2014-10-21 14:22:26 +0200

    format check for passwords

--- ETSN05/BaseBlockSystem/src/web/UserList.java
+++ ETSN05/BaseBlockSystem/src/web/UserList.java
@@ -28,122 +28,134 @@ import java.util.Random;
 public class UserList extends servletBase {
 	private static final long serialVersionUID = 1L;
 	private static final int PASSWORD_LENGTH = 6;
-       
-    /**
-     * @see servletBase#servletBase()
-     */
-    public UserList() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    /**
-     * generates a form for adding new users
-     * @return HTML code for the form
-     */
-    private String addUserForm() {
-    	String html;
-    	html = "<p>Add new user: <br><table border=" + formElement("1") + "><tr><td>";
-    	html += "<form name=" + formElement("input");
-    	html += " method=" + formElement("get");
-    	html += "<p> User name: <input type=" + formElement("text") + " name=" + formElement("addname") + '>';    	
-    	html += "<p> First name: <input type=" + formElement("text") + " name=" + formElement("firstname") + '>';    	
-    	html += "<p> Last name: <input type=" + formElement("text") + " name=" + formElement("lastname") + '>';    	    	
-    	html += "<input type=" + formElement("submit") + "value=" + formElement("Add user") + '>';
-    	html += "</form></td></tr></table><br>";
-    	return html;
-    }
-    
-    private String addChangePWForm(String name) {
-    	String html;
-    	html = "<p><form name=" + formElement("input");
-    	html += " method=" + formElement("get");
-    	html += "<p><input type=" + formElement("text") + " name=" + formElement("password") + '>';    	
-    	html += "<input type='hidden' name=" + formElement("name") + " value=" + formElement(name) + '>';
-    	html += "<input type=" + formElement("submit") + " value='Change PW'>";
-    	html += "</form>";
-    	return html;
-    }
-    
-    /**
-     * Checks if a username corresponds to the requirements for user names. 
-     * @param name The investigated username
-     * @return True if the username corresponds to the requirements
-     */
-    private boolean checkNewName(String name) {
-    	int length = name.length();
-    	boolean ok = (length>=5 && length<=10);
-    	if (ok)
-    		for (int i=0; i<length; i++) {
-    			int ci = (int)name.charAt(i);
-    			boolean thisOk = ((ci>=48 && ci<=57) || 
-    					          (ci>=65 && ci<=90) ||
-    					          (ci>=97 && ci<=122));
-    			//String extra = (thisOk ? "OK" : "notOK");
-    			//System.out.println("bokst:" + name.charAt(i) + " " + (int)name.charAt(i) + " " + extra);
-    			ok = ok && thisOk;
-    		}    	
-    	return ok;
-    }
-    
-    /**
-     * Creates a random password.
-     * @return a randomly chosen password
-     */
-    private String createPassword() {
-    	String result = "";
-    	Random r = new Random();
-    	for (int i=0; i<PASSWORD_LENGTH; i++)
-    		result += (char)(r.nextInt(26)+97); // 122-97+1=26
-    	return result;
-    }
-    
-    
-    /**
-     * Adds a user and a randomly generated password to the database.
-     * @param name Name to be added
-     * @return true if it was possible to add the name. False if it was not, e.g. 
-     * because the name already exist in the database. 
-     */
-    private boolean addUser(String name, String firstName, String lastName) {
-    	boolean resultOk = true;
-    	try{
+
+	/**
+	 * @see servletBase#servletBase()
+	 */
+	public UserList() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * generates a form for adding new users
+	 * @return HTML code for the form
+	 */
+	private String addUserForm() {
+		String html;
+		html = "<p>Add new user: <br><table border=" + formElement("1") + "><tr><td>";
+		html += "<form name=" + formElement("input");
+		html += " method=" + formElement("get");
+		html += "<p> User name: <input type=" + formElement("text") + " name=" + formElement("addname") + '>';    	
+		html += "<p> First name: <input type=" + formElement("text") + " name=" + formElement("firstname") + '>';    	
+		html += "<p> Last name: <input type=" + formElement("text") + " name=" + formElement("lastname") + '>';    	    	
+		html += "<input type=" + formElement("submit") + "value=" + formElement("Add user") + '>';
+		html += "</form></td></tr></table><br>";
+		return html;
+	}
+
+	private String addChangePWForm(String name) {
+		String html;
+		html = "<p><form name=" + formElement("input");
+		html += " method=" + formElement("get");
+		html += "<p><input type=" + formElement("text") + " name=" + formElement("password") + '>';    	
+		html += "<input type='hidden' name=" + formElement("name") + " value=" + formElement(name) + '>';
+		html += "<input type=" + formElement("submit") + " value='Change PW'>";
+		html += "</form>";
+		return html;
+	}
+
+	/**
+	 * Checks if a username corresponds to the requirements for user names. 
+	 * @param name The investigated username
+	 * @return True if the username corresponds to the requirements
+	 */
+	private boolean checkNewName(String name) {
+		int length = name.length();
+		boolean ok = (length>=5 && length<=10);
+		if (ok)
+			for (int i=0; i<length; i++) {
+				int ci = (int)name.charAt(i);
+				boolean thisOk = ((ci>=48 && ci<=57) || 
+						(ci>=65 && ci<=90) ||
+						(ci>=97 && ci<=122));
+				//String extra = (thisOk ? "OK" : "notOK");
+				//System.out.println("bokst:" + name.charAt(i) + " " + (int)name.charAt(i) + " " + extra);
+				ok = ok && thisOk;
+			}    	
+		return ok;
+	}
+
+	private boolean checkNewPassword(String name) {
+		int length = name.length();
+		boolean ok = (length==6);
+		if (ok)
+			for (int i=0; i<length; i++) {
+				int ci = (int)name.charAt(i);
+				boolean thisOk = (ci>=97 && ci<=122);
+				ok = ok && thisOk;
+			}    	
+		return ok;
+	}
+
+	/**
+	 * Creates a random password.
+	 * @return a randomly chosen password
+	 */
+	private String createPassword() {
+		String result = "";
+		Random r = new Random();
+		for (int i=0; i<PASSWORD_LENGTH; i++)
+			result += (char)(r.nextInt(26)+97); // 122-97+1=26
+		return result;
+	}
+
+
+	/**
+	 * Adds a user and a randomly generated password to the database.
+	 * @param name Name to be added
+	 * @return true if it was possible to add the name. False if it was not, e.g. 
+	 * because the name already exist in the database. 
+	 */
+	private boolean addUser(String name, String firstName, String lastName) {
+		boolean resultOk = true;
+		try{
 			Statement stmt = conn.createStatement();
 			String statement = "insert into users (username, firstname, lastname, password) values('" + name + "', '" + firstName + "',"
 					+ "'" + lastName + "', '" + 
-			                     createPassword() + "')";
+					createPassword() + "')";
 			System.out.println(statement);
-		    stmt.executeUpdate(statement); 
-		    stmt.close();
-			
+			stmt.executeUpdate(statement); 
+			stmt.close();
+
 		} catch (SQLException ex) {
-		    resultOk = false;
-		    // System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+			resultOk = false;
+			// System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
-    	return resultOk;
-    }
-    
-    /**
-     * Deletes a user from the database. 
-     * If the user does not exist in the database nothing happens. 
-     * @param name name of user to be deleted. 
-     */
-    private void deleteUser(String name) {
-    	try{
+		return resultOk;
+	}
+
+	/**
+	 * Deletes a user from the database. 
+	 * If the user does not exist in the database nothing happens. 
+	 * @param name name of user to be deleted. 
+	 */
+	private void deleteUser(String name) {
+		try{
 			Statement stmt = conn.createStatement();
 			String statement = "delete from users where username='" + name + "'"; 
 			System.out.println(statement);
-		    stmt.executeUpdate(statement); 
-		    stmt.close();
-			
+			stmt.executeUpdate(statement); 
+			stmt.close();
+
 		} catch (SQLException ex) {
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
-    }
+	}
 
 
 	/**
@@ -160,13 +172,13 @@ public class UserList extends servletBase {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println(getPageIntro());
-		
+
 		String myName = "";
-    	HttpSession session = request.getSession(true);
-    	Object nameObj = session.getAttribute("name");
-    	if (nameObj != null)
-    		myName = (String)nameObj;  // if the name exists typecast the name to a string
-		
+		HttpSession session = request.getSession(true);
+		Object nameObj = session.getAttribute("name");
+		if (nameObj != null)
+			myName = (String)nameObj;  // if the name exists typecast the name to a string
+
 		// check that the user is logged in
 		if (!loggedIn(request))
 			response.sendRedirect("LogIn");
@@ -176,22 +188,26 @@ public class UserList extends servletBase {
 				String cpwUser = request.getParameter("name");
 				if (cpwPass != null){
 					Statement stmt;
-					try {
-						stmt = conn.createStatement();
-					    int result = stmt.executeUpdate("update users set password='" + cpwPass + "' where username='" + cpwUser + "'");
-					    if (result==0){
-					    	out.println("Password change failed.");
-					    }else{
-					    	out.println("Password change successful.");
-					    }
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}		 
+					if (checkNewPassword(cpwPass)){
+						try {
+							stmt = conn.createStatement();
+							int result = stmt.executeUpdate("update users set password='" + cpwPass + "' where username='" + cpwUser + "'");
+							if (result==0){
+								out.println("Password change failed.");
+							}else{
+								out.println("Password change successful.");
+							}
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}		 
+					}else{
+						out.println("Password change failed: password must be 6 character with ASCII code 97-122.");
+					}
 				}
 				out.println("<h1>Administration page " + "</h1>");
-				
+
 				out.println(addUserForm());
-				
+
 				// check if the administrator wants to add a new user in the form
 				System.out.println(request.getParameter("password"));
 				System.out.println(request.getParameter("name"));
@@ -206,7 +222,7 @@ public class UserList extends servletBase {
 					}	else
 						out.println("<p>Error: Suggested name not allowed</p>");
 				}
-					
+
 				// check if the administrator wants to delete a user by clicking the URL in the list
 				String deleteName = request.getParameter("deletename");
 				if (deleteName != null) {
@@ -215,42 +231,42 @@ public class UserList extends servletBase {
 					}	else
 						out.println("<p>Error: URL wrong</p>");
 				}
-				
+
 				try {
 					Statement stmt = conn.createStatement();		    
-				    ResultSet rs = stmt.executeQuery("select * from users order by username asc");
-				    out.println("<p>Registered users:</p>");
-				    out.println("<table border=" + formElement("1") + ">");
-				    out.println("<tr><td>USER NAME</td><td>PASSWORD</td><td>FIRST NAME</td><td>LAST NAME</td></tr>");
-				    while (rs.next( )) {
-				    	String name = rs.getString("username");
-				    	String pw = rs.getString("password");
-				    	String fname = rs.getString("firstname");
-				    	String lname = rs.getString("lastname");
-				    	String deleteURL = "UserList?deletename="+name;
-				    	String deleteCode = "<a href=" + formElement(deleteURL) +
-				    			            " onclick="+formElement("return confirm('Are you sure you want to delete "+name+"?')") + 
-				    			            "> delete </a>";
-				    	String cpwCode = addChangePWForm(name);
-				    	if (name.equals("admin")){
-				    		deleteCode = "";
-				    		cpwCode = "";
-				    	}
-				    	out.println("<tr>");
-				    	out.println("<td>" + name + "</td>");
-				    	out.println("<td>" + pw + "</td>");
-				    	out.println("<td>" + fname + "</td>");
-				    	out.println("<td>" + lname + "</td>");
-				    	out.println("<td>" + cpwCode + "</td>");
-				    	out.println("<td>" + deleteCode + "</td>");
-				    	out.println("</tr>");
-				    }
-				    out.println("</table>");
-				    stmt.close();
+					ResultSet rs = stmt.executeQuery("select * from users order by username asc");
+					out.println("<p>Registered users:</p>");
+					out.println("<table border=" + formElement("1") + ">");
+					out.println("<tr><td>USER NAME</td><td>PASSWORD</td><td>FIRST NAME</td><td>LAST NAME</td></tr>");
+					while (rs.next( )) {
+						String name = rs.getString("username");
+						String pw = rs.getString("password");
+						String fname = rs.getString("firstname");
+						String lname = rs.getString("lastname");
+						String deleteURL = "UserList?deletename="+name;
+						String deleteCode = "<a href=" + formElement(deleteURL) +
+								" onclick="+formElement("return confirm('Are you sure you want to delete "+name+"?')") + 
+								"> delete </a>";
+						String cpwCode = addChangePWForm(name);
+						if (name.equals("admin")){
+							deleteCode = "";
+							cpwCode = "";
+						}
+						out.println("<tr>");
+						out.println("<td>" + name + "</td>");
+						out.println("<td>" + pw + "</td>");
+						out.println("<td>" + fname + "</td>");
+						out.println("<td>" + lname + "</td>");
+						out.println("<td>" + cpwCode + "</td>");
+						out.println("<td>" + deleteCode + "</td>");
+						out.println("</tr>");
+					}
+					out.println("</table>");
+					stmt.close();
 				} catch (SQLException ex) {
-				    System.out.println("SQLException: " + ex.getMessage());
-				    System.out.println("SQLState: " + ex.getSQLState());
-				    System.out.println("VendorError: " + ex.getErrorCode());
+					System.out.println("SQLException: " + ex.getMessage());
+					System.out.println("SQLState: " + ex.getSQLState());
+					System.out.println("VendorError: " + ex.getErrorCode());
 				}
 				out.println("</body></html>");
 			} else  // name not admin
